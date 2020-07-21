9607ac37dd11a39a850f9922be1771155fb9d166
Ibrahim Zbib <abraham.zbib@gmail.com>
2020-07-21 14:38:08 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CardCreate from './CreateCard/CardCreate';
+
+ReactDOM.render( <CardCreate  />, document.getElementById('root'));
